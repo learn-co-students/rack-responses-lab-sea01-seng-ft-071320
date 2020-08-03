cffaa2dfa534416dde1f78c0b28b1adf361e338e
LucasLeibs <dr.lucasleibs@gmail.com>
2020-08-03 11:24:35 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application 
     def call(env)
         resp = Rack::Response.new
-        time = Time.now.hour
+        time = Time.now.strftime("%k").to_i
 
         if  time <= 12
             resp.write "Good Morning!"
